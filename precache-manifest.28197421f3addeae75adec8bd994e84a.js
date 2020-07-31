01001e6e5ab0e6d8bec9415033455ff173eb32ca
PremKishore-10 <kishorepre97@gmail.com>
2020-08-01 00:04:45 +0530

    Updates

---
+++ precache-manifest.28197421f3addeae75adec8bd994e84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9be441ea977b929807c73a1430ec9a1",
+    "url": "/Demo/index.html"
+  },
+  {
+    "revision": "b0c9589afa3e3aa98fc1",
+    "url": "/Demo/static/css/main.dad01531.chunk.css"
+  },
+  {
+    "revision": "6c2b6c9d1cc2784126ee",
+    "url": "/Demo/static/js/2.a5c2ccff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demo/static/js/2.a5c2ccff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c9589afa3e3aa98fc1",
+    "url": "/Demo/static/js/main.6d35b7cd.chunk.js"
+  },
+  {
+    "revision": "8f44eee9180bf04d5a5e",
+    "url": "/Demo/static/js/runtime-main.9fadc558.js"
+  }
+]);
